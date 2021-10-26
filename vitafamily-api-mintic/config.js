2383176f411285ae1d944f05a2d40ddce8e76dcb
Johan Ussa Rubio <johanuss0405@gmail.com>
2021-10-26 17:38:23 -0500

    Implementacion de MongoAtlas

--- vitafamily-api-mintic/config.js
+++ vitafamily-api-mintic/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/vitafamily'
+    db: process.env.MONGODB || 'mongodb+srv://admin:12345@vitafamilydb.iq2cq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
